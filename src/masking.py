adf8f9a47ad2c5a3c085c934b107a72930f2914d
f-tominaga <tommy.tommy.walker@gmail.com>
2025-01-20 16:27:51 +0000

    complete masking

--- src/masking.py
+++ src/masking.py
@@ -6,16 +6,18 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 
-
-
 # Node name
 NODE_NAME = 'masking_node'
 
 bridge = CvBridge()
 pub = rospy.Publisher('/'+NODE_NAME+'/output_image', Image, queue_size=5)
 
+raw = None
+mask = None
+
 def callback(msg_raw, msg_mask):
   rospy.loginfo('callback')
+  
   raw = bridge.imgmsg_to_cv2(msg_raw)
   mask = bridge.imgmsg_to_cv2(msg_mask)
   
@@ -24,18 +26,39 @@ def callback(msg_raw, msg_mask):
   msg_ouptut = bridge.cv2_to_imgmsg(output, 'bgr8')
   
   pub.Publish(msg_output)
+  return
+
+def callback_raw(msg):
+  # rospy.loginfo('callback raw image')
   
-if __name__=="__main__":
-  rospy.init_node(NODE_NAME, anonymous=False)
+  global raw
+  raw = bridge.imgmsg_to_cv2(msg)
+
+def callback_mask(msg):
+  # rospy.loginfo('callback mask')
+
+  global raw
+  global mask
+  mask = bridge.imgmsg_to_cv2(msg)
   
-  sub_raw = message_filters.Subscriber('/'+NODE_NAME+'/raw_image', Image)
-  sub_mask = message_filters.Subscriber('/'+NODE_NAME+'/mask_image', Image)
-  
-  
-  fps = 100.
-  delay = 1 / fps * 0.5
-  
-  mf = message_filters.ApproximateTimeSynchronizer([sub_raw, sub_mask], 10, delay)
-  mf.registerCallback(callback)
-  
-  rospy.spin()
+  output = cv2.bitwise_and(raw, raw, mask=mask)
+  msg_output = bridge.cv2_to_imgmsg(output, 'bgr8')
+  pub.publish(msg_output)
+
+
+rospy.init_node(NODE_NAME, anonymous=False)
+
+# sub_raw = message_filters.Subscriber('/raw', Image)
+# sub_mask = message_filters.Subscriber('/mask', Image)
+
+# fps = 1.
+# delay = 1 / fps * 0.5
+
+# rospy.loginfo('test')
+# mf = message_filters.ApproximateTimeSynchronizer([sub_raw, sub_mask], 10, 0.5, allow_headerless=True)
+# mf.registerCallback(callback)
+
+sub_raw = rospy.Subscriber('raw', Image, callback_raw)
+sub_mask = rospy.Subscriber('mask', Image, callback_mask)
+
+rospy.spin()
